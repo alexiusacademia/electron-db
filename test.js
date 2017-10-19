d5911133e34bbabc10dde2693d3b9c60f501528a
syncster31 <alexius.academia@gmail.com>
2017-10-19 20:47:43 +0800

    Deleted test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const db = require('./index.js');
-const electron = require('electron');
-
-const app = electron.app;
-
-db.getRow('customers', 1508371528701, (succ, msg) => {
-  console.log("Success: " + succ);
-  console.log("Message: " + msg.name);
-})
