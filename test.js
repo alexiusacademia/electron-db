168d05d128e543a32d6f22b1e42d8369f9d08eb9
syncster31 <alexius.academia@gmail.com>
2017-10-19 19:58:43 +0800

    Edit test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const db = require('./index.js');
 const electron = require('electron');
 
-const app = electron.app || electron.remote.app;
+const app = electron.app;
 
 db.getRow('customers', 1508371528701, (succ, msg) => {
   console.log("Success: " + succ);
